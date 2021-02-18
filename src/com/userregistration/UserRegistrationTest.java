cb4a93aba35637004e1dd33c8b2a561d3506a62d
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-18 13:45:33 +0530

    validate email

--- src/com/userregistration/UserRegistrationTest.java
+++ src/com/userregistration/UserRegistrationTest.java
@@ -25,7 +25,19 @@ public class UserRegistrationTest {
     @Test
     public void givenLastName_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateLastName("ch");
+        boolean result = userValidater.validateLastName("Ch");
+        Assert.assertEquals(false,result);
+    }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturn_True() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateEmail("abc.xyz@bl.co.in");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturn_False() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateEmail("vinayak@.com");
         Assert.assertEquals(false,result);
     }
 }
--- src/com/userregistration/UserValidater.java
+++ src/com/userregistration/UserValidater.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserValidater {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -13,5 +14,10 @@ public class UserValidater {
     public boolean validateLastName(String lastName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lastName).matches();
+    }
+
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
